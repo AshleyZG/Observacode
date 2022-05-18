4bb4db8e4dde398f776b2db4cdce8bc245851527
AshleyZG <gsarazh@gmail.com>
2022-05-18 14:07:46 -0400

    initialize extension

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the observacode extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'observacode:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension observacode is activated!');
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The observacode server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
